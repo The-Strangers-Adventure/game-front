19a564f2e5a81e26d043ff99902538aec0bcee57
Brenden <brendenseifried@gmail.com>
2022-10-16 11:45:34 -0700

    added prooper function parameters, unfortuantly hp always resets which will need work and maybe some consants

--- src/components/battlefunctions.js
+++ src/components/battlefunctions.js
@@ -3,29 +3,31 @@
 
 
 
-let hp = 100;
+// let hp = 100;
 ////////////////Pick Hp if 0 OR below it will log out defeated enemy, otherwise it will log remaining health.
 // let hp = 0;
 
 ///////////////This is assuming a drop down is selected or a roll is made, it is just a way of showing different degrees of damage can be calculated out at once without big if statements.
-let pick = 'punch';
+// let pick = 'punch';
 // let pick = 'kick';
 // let pick = 'stab';
 // let pick = null;
 
-function attackSwithCaseFunct(){  
-
-////////////////While loop allows this to run only whole enemy has hp to give.
+function attackSwithCaseFunct(hp, attack){  
+////////////////While loop allows this to run only while enemy has hp to give.
     while (hp > 0){
-    switch(pick){
+    switch(attack){
         case 'punch':
             // return hp - 10;
 ////////////////A calculation that returns a random damage amount within a range, in this case between 5-15 dmg
-              return Math.ceil(Math.random() * (15 - 5) + 5);
+               damage = Math.ceil(Math.random() * (15 - 5) + 5);
+               console.log(damage, 'damage')
+              return hp - damage
             break;
         case 'kick':
             // return hp - 20;
-            return Math.ceil(Math.random() * (30 - 15) + 15);
+            damage = Math.ceil(Math.random() * (30 - 15) + 15);
+           return hp - damage
 ///////////////not exactly sure why 'break' isn't highlighted, may need to update vscode. From what I understand break makes sure the code stops running when it finds the right selection instead of checking against every variable.
             break;
         case 'stab':
@@ -40,18 +42,19 @@ function attackSwithCaseFunct(){
         default: 
         return 'Miss!'
 
-    }
-}
+    }}
  if (hp <= 0) 
  {
-    ////not TOOOO sure if this if statement is needed. I have a strong feeling it can be condensed into the switch statement but I thought about making this last minute and its pretty late.
+    ////not TOO sure if this if statement is needed. I have a strong feeling it can be condensed into the switch statement but I thought about making this last minute and its pretty late.
     return 'Enemy Defeated'
 } else 
 {
     //////return the output
-    return pick
+    return hp
 }
 }
 
 ///////read the output 
-console.log(attackSwithCaseFunct())
+console.log(attackSwithCaseFunct(100, 'punch'))
+
+/////////you can hoose 'punch', 'kick', or 'stab', anything else is a miss. if hp = 0 then it will read a message saying enemy is dead.
